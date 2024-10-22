42ad8ae16b0bd37b211e3823403569e1e496f636
Administrator <deeppatel100267@gmail.com>
2024-10-22 23:50:26 +0530

    10/22/24

--- lib/Hidden Gallery App/screens/home/home_page.dart
+++ lib/Hidden Gallery App/screens/home/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import '../../utils/list_image.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,12 +10,40 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              CupertinoIcons.lock,
+              color: Colors.white,
+            ),
+          )
+        ],
         elevation: 7,
         shadowColor: Colors.black87,
         backgroundColor: const Color(0xff070F2B),
         title: const Text(
           'Gallery',
           style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: GridView.builder(
+        itemCount: 18,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+        ),
+        itemBuilder: (context, index) => Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            height: 100,
+            width: 100,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(image[index]),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
         ),
       ),
     );
---
+++ lib/Hidden Gallery App/utils/list_image.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+List image = [
+  'https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg',
+  'https://gratisography.com/wp-content/uploads/2024/03/gratisography-funflower-800x525.jpg',
+  'https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg',
+  'https://i0.wp.com/picjumbo.com/wp-content/uploads/silhouette-of-a-guy-with-a-cap-at-red-sky-sunset-free-image.jpeg?h=800&quality=80',
+  'https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?cs=srgb&dl=pexels-anjana-c-169994-674010.jpg&fm=jpg',
+  'https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/enhancer/2.jpg',
+  'https://pixlr.com/images/generator/text-to-image.webp',
+  'https://cdn.prod.website-files.com/62d84e447b4f9e7263d31e94/6399a4d27711a5ad2c9bf5cd_ben-sweet-2LowviVHZ-E-unsplash-1.jpeg',
+  'https://thumbs.dreamstime.com/b/falling-drop-rain-blue-earth-image-water-splash-crown-shape-water-splash-crown-shape-falling-drop-earth-140453719.jpg',
+  'https://media.wired.com/photos/65382632fd3d190c7a1f5c68/master/pass/Google-Image-Search-news-Gear-GettyImages-824179306.jpg',
+  'https://imagekit.io/blog/content/images/2019/12/image-optimization.jpg',
+  'https://thumbs.dreamstime.com/b/young-conceptual-image-large-stone-shape-human-brain-conceptual-image-large-stone-shape-110748113.jpg',
+  'https://wallpapers.com/images/featured/image-pictures-79gc4p3mqu7an848.jpg',
+  'https://img.freepik.com/premium-photo/image-colorful-galaxy-sky-generative-ai_791316-9864.jpg',
+  'https://letsenhance.io/static/66c1b6abf8f7cf44c19185254d7adb0c/28ebd/AiArtBefore.jpg',
+  'https://i0.wp.com/picjumbo.com/wp-content/uploads/amazing-stone-path-in-forest-free-image.jpg?w=600&quality=80',
+  'https://www.w3schools.com/howto/img_forest.jpg',
+  'https://st.depositphotos.com/1307373/3789/i/450/depositphotos_37896767-stock-photo-love-ladybug-sitting-on-the.jpg',
+];
